0d750e24d737360935b2919d5c4e773781134cd2
brendanburke <bfburke33@gmail.com>
2022-06-08 13:27:30 -0400

    listings post route is working, AWS is not working yet

--- src/pages/NewListing.jsx
+++ src/pages/NewListing.jsx
@@ -1,8 +1,84 @@
 import React from "react";
+import { useState, useEffect } from "react";
 
 const NewListing = () => {
+
+    const [listings, setListings] = useState(null)
+    const URL = 'https://instrument-swap-backend.herokuapp.com/listings'
+
+    const [newForm, setNewForm] = useState({
+        title: '',
+        description: '',
+        image: '',
+    })
+
+    const handleChange = (e) => {
+        setNewForm({...newForm, [e.target.name]: e.target.value})
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        createListing(newForm);
+        setNewForm({
+            title: '',
+            description: '',
+            image: '',
+        })
+    }
+
+    const getListings = async () => {
+        const response = await fetch(URL)
+        const data = await response.json()
+        setListings(data)
+        console.log(data)
+    };
+    useEffect(() => {
+        getListings()
+        
+    }, [])
+
+    const createListing = async (listing) => {
+        await fetch(URL, {
+            method: "post",
+            headers: {
+                "Content-Type": "application/json",
+            },
+                body: JSON.stringify(listing),
+        });
+        getListings();
+    };
+
+    
+
+
     return (
-        <h2>NewListing Page</h2>
+        <section>
+        <form onSubmit={handleSubmit}>
+            <input
+                type="text"
+                value={newForm.title}
+                name="title"
+                placeholder="name"
+                onChange={handleChange}
+            />
+            <input
+                type="text"
+                value={newForm.description}
+                name="description"
+                placeholder="title"
+                onChange={handleChange}
+            />
+            <input
+                type="text"
+                value={newForm.image}
+                name="image"
+                placeholder="image URL"
+                onChange={handleChange}
+            />
+            <input type="submit" value="Create Listing" />
+        </form>
+        {/* {listings ? loaded() : loading()} */}
+    </section>
     )
 }
 
