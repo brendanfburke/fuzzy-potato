f1dd2ec6cd946e001bd7757c7d59ccda3c791557
brendanburke <bfburke33@gmail.com>
2022-06-09 18:33:14 -0400

    working on login function

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,15 +1,87 @@
 import React from "react";
 import { Button, Form, Card } from "react-bootstrap/esm";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router";
+import { useState, useEffect } from "react";
 
 const Login = () => {
+
+    let navigate = useNavigate()
+
+    // const goHome = () => {
+    //     let path='/'
+    //     navigate(path)
+    // }
+
+    const [user, setUser] = useState(null)
+    const URL = 'https://instrument-swap-backend.herokuapp.com/login'
+    const URL2 = 'https://instrument-swap-backend.herokuapp.com/users'
+
+    const [newForm, setNewForm] = useState({
+        username: '',
+        password: ''
+    })
+
+    const handleChange = (e) => {
+        setNewForm({...newForm, [e.target.name]: e.target.value})
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        loginRequest(newForm);
+        setNewForm({
+            username: '',
+            password: '',
+        })
+    }
+
+    // const getUsers = async () => {
+    //     const response = await fetch(URL)
+    //     const data = await response.json()
+    //     setUser(data)
+    //     console.log(data)
+    // };
+    // useEffect(() => {
+    //     getUsers()
+        
+    // }, [])
+
+    const loginRequest = async (userAccount) => {
+        await fetch(URL, {
+            method: "post",
+            headers: {
+                "Content-Type": "application/json",
+            },
+                body: JSON.stringify(userAccount),
+        }) 
+        .then((response) => {
+            console.log(response.data)
+        })
+        
+    };
+
     return (
         <div className="login">
-                <Form>
-                    <Form.Control type='username' placeholder='Enter your username here'></Form.Control>
-                    <Form.Control type='password' placeholder='Password'></Form.Control>
-                    <Button className="mt-3" variant='primary' type='submit' >Login</Button>
-                </Form>
+            <form onSubmit={handleSubmit} className='new-account-form'>
+                <label htmlFor="user_id">Username</label>
+                <input
+                    type="text"
+                    value={newForm.username}
+                    name="username"
+                    placeholder="Ex: georgeharrison"
+                    onChange={handleChange}
+                />
+                <label htmlFor="first_name">Password</label>
+                <input
+                    type="text"
+                    value={newForm.password}
+                    name="password"
+                    placeholder="Ex: beatle2"
+                    onChange={handleChange}
+                />
+                
+                <input type="submit" value="Login" />
+        </form>
             <p>Don't have an account? Create one <Link to='/register'>here</Link></p>
         </div>
     )
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 import { Button, Form, Card } from "react-bootstrap/esm";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router";
 
 const Register = () => {
+
+
+    const navigate = useNavigate()
+
+    const goLogin = () => {
+        let path = '/login'
+        navigate(path)
+    }
+
     return (
         <div className="register">
                 <Form>
@@ -10,7 +20,7 @@ const Register = () => {
                     <Form.Control type='username' placeholder='Ex: guitarseller33'></Form.Control>
                     <Form.Label>Create a unique password that you won't forget</Form.Label>
                     <Form.Control type='password' placeholder='Ex: ILoveGuitars44!'></Form.Control>
-                    <Button className="mt-3" variant='primary' type='submit' >Create Account</Button>
+                    <Button className="mt-3" onClick={goLogin} variant='primary' type='submit' >Create Account</Button>
                 </Form>
             <p>Already have an account? Return to the <Link to='/login'>Login Page</Link></p>
         </div>
