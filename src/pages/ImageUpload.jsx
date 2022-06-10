0b2d60a625b2a72d9170ff1e27aa92e19f45b1b7
brendanburke <bfburke33@gmail.com>
2022-06-10 11:58:22 -0400

    rebooting vscode

--- src/pages/ImageUpload.jsx
+++ src/pages/ImageUpload.jsx
@@ -1,17 +1,59 @@
 import React from "react";
 import { useParams } from "react-router";
+import { useState } from "react";
+import Axios from "axios";
 
 const ImageUpload = () => {
     const params = useParams()
 
+    const URL = 'https://instrument-swap-backend.herokuapp.com/single'
+
+    const [newForm, setNewForm] = useState(null)
+
+    const handleChange = (e) => {
+        setNewForm(e.target.file[0])
+        console.log(e.target.files[0])
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        imageSubmit(newForm);
+        setNewForm({
+            username: '',
+            password: '',
+        })
+    }
+
+
+
+    const imageSubmit = async () => {
+        await Axios.post(URL, newForm, {mode: 'cors'}).then((response) => {
+            console.log(response)
+            // if (response) {
+            // } else {
+            //     warningMessage = 'incorrect username or password, please try again or register a new account'
+            // }
+        })
+    }
+
     return (
-        <div className="image-upload-container">
-            <p>{params.id}</p>
-            <form action="">
-                <input type="file" />
+        <div className="login">
+            <form onSubmit={handleSubmit}  className='new-account-form'>
+                <input type="file" onChange={handleChange} /> 
+                <button>Upload</button>
             </form>
+
         </div>
     )
+
+    // return (
+    //     <div className="image-upload-container">
+    //         <p>{params.id}</p>
+    //         <form action="">
+    //             <input type="file" />
+    //         </form>
+    //     </div>
+    // )
 }
 
 export default ImageUpload
